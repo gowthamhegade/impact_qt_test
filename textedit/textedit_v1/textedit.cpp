a071d2e1880854899a9ab674770fcea35ea7344c
gowthamhegade <gowtham.hegde@algoshack.com>
2024-03-06 15:07:02 +0530

    previous text
    previous text

--- textedit/textedit_v1/textedit.cpp
+++ textedit/textedit_v1/textedit.cpp
@@ -58,7 +58,6 @@ const QString rsrcPath = ":/images";
    setCentralWidget(textEdit);
    textEdit->setFocus();
    setCurrentFileName(QString());
-   cout << "Hello World!";
 
    fontChanged(textEdit->font());
    colorChanged(textEdit->textColor());
