b54f805016de9cf49bce4c38d6e49cba2aef77b5
gowthamhegade <gowtham.hegde@algoshack.com>
2024-03-26 12:34:17 +0530

    fresh

--- impactbasedtesting/database.py
+++ impactbasedtesting/database.py
@@ -3,6 +3,6 @@ RESULT_PATH = REPO_PATH + "\\impactbasedtesting\\results"
 EXECUTABLE_PATH = REPO_PATH + "\\textedit\\textedit_v1\\textedit.exe"
 TEXTEDIT_PATH = REPO_PATH + "\\textedit"
 TEST_SCRIPTS_PATH = REPO_PATH + "\\textedit\\suite_TextEditSuite"
-START_COMMIT = "0f65dd6e2b7fae59a1eb1c691a0bb70897e98612"
+START_COMMIT = "a0148e1918b011f637e4731890082d99a8eaffc3"
 # DATA_PATH = REPO_PATH + "\\impactbasedtesting\\data"
 input_data = REPO_PATH + '\\impactbasedtesting\\data\\'
